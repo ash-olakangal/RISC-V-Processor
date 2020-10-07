c951d393b42ca70f5f0d9d3a0a919bd5827f895b
Ashwin-Olakangal <ashwin.olakangal@gmail.com>
2020-10-07 18:29:33 +0530

    ALU module

---
+++ ALU/ALU.v
@@ -0,0 +1,48 @@
+/*
+ALU module, which takes two operands of size 32-bits each and a 4-bit ALU_control as input.
+Operation is performed on the basis of ALU_control value and output is 32-bit ALU_result. 
+If the ALU_result is zero, a ZERO FLAG is set.
+*/
+
+/*
+ALU Control lines | Function
+-----------------------------
+        0000    Bitwise-AND
+        0001    Bitwise-OR
+        0010	Add (A+B)
+        0100	Subtract (A-B)
+        1000	Set on less than
+*/
+
+module ALU (
+    input [31:0] in1,in2, 
+    input[3:0] alu_control,
+    output reg [31:0] alu_result,
+    output reg zero_flag
+);
+    always @(*)
+    begin
+        // Operating based on control input
+        case(alu_control)
+
+        4'b0000: alu_result = in1&in2;
+        4'b0001: alu_result = in1|in2;
+        4'b0010: alu_result = in1+in2;
+        4'b0100: alu_result = in1-in2;
+        4'b1000: begin 
+            if(in1<in2)
+            alu_result = 1;
+            else
+            alu_result = 0;
+        end 
+
+        endcase
+
+        // Setting Zero_flag if ALU_result is zero
+        if (alu_result == 0)
+            zero_flag = 1'b1;
+        else
+            zero_flag = 1'b0;
+        
+    end
+endmodule
---
+++ ALU/ALU_testbench.v
@@ -0,0 +1,39 @@
+`include "ALU.v"
+
+module stimulus ();
+    reg [31:0] A,B;
+    reg [3:0] ALUControl;
+    wire ZERO;
+    wire [31:0] ALUResult;
+
+    // Instantiating modules
+    ALU test_mod(.in1(A),.in2(B),.alu_control(ALUControl),.zero_flag(ZERO),.alu_result(ALUResult));
+
+    // Setting up waveform
+    initial
+    begin
+        $dumpfile("output_wave.vcd");
+        $dumpvars(0,stimulus);
+    end
+
+    // Monitoring changing values
+    initial
+    $monitor($time, "\nInput_1 = %b, \nInput_2 = %b,\nALU_control = %b,\n ALU_result = %b, Zero_flag = %b\n",A,B,ALUControl,ALUResult,ZERO);
+
+    // Test conditions
+    initial
+    begin
+        A = 23; B = 42;  ALUControl = 4'b0000;
+    #20 A = 23; B = 42;  ALUControl = 4'b0001;
+    #20 A = 23; B = 42;  ALUControl = 4'b0010;
+    #20 A = 23; B = 42;  ALUControl = 4'b0100;
+    #20 A = 23; B = 42;  ALUControl = 4'b1000;
+    #20 A = 42; B = 23;  ALUControl = 4'b1000;
+    #20 A = 42; B = 23;  ALUControl = 4'b0100;
+    end
+
+    // Finish after 150 clock cycles
+    initial
+    #150 $finish;
+
+endmodule
