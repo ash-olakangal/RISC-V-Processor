f77bed223cf3b3caa8bf2aa2f2f92f75c0d903e7
Ashwin-Olakangal <ashwin.olakangal@gmail.com>
2020-10-08 22:46:26 +0530

    Instruction memory

---
+++ Instruction Fetch Unit/Instruction mem_tb.v
@@ -0,0 +1,36 @@
+`include "Instruction memory.v"
+
+module stimulus ();
+    reg [31:0] PC;
+    reg RESET;
+    wire [31:0]INST_CODE;
+
+    // Instantiating instruction memory module
+    Instruction_Memory test_mod(PC,RESET,INST_CODE);
+
+    // Setting up waveform
+    initial
+    begin
+        $dumpfile("output_wave.vcd");
+        $dumpvars(0,stimulus);
+    end
+
+    initial
+    $monitor($time, " Instruction code = %h, PC = %d", INST_CODE, PC);
+
+    // Setting test conditions
+    initial
+    begin
+        RESET = 1'b0;
+        #20 RESET = 1'b1;
+        #20 PC = 32'd0;
+        #20 PC = 32'd4;
+        #20 PC = 32'd8;
+        #20 PC = 32'd12;
+    end
+
+    // Terminating after 150 clock cycles
+    initial
+    #150 $finish;
+
+endmodule
