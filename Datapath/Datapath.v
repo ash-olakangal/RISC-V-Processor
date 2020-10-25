739f2a300dd4f6cfcf689c1c0466dc018aa63780
Ashwin-Olakangal <ashwin.olakangal@gmail.com>
2020-10-25 18:54:33 +0530

    Datapath implementation

---
+++ Datapath/Datapath.v
@@ -0,0 +1,36 @@
+`include "./../Register file/Register File.v"
+`include "./../ALU/ALU.v"
+
+module Datapath (
+    input [4:0]read_reg_num1,
+    input [4:0]read_reg_num2,
+    input [4:0]write_reg,
+    input [3:0]alu_control,
+    input regwrite,
+    input clock,
+    input reset,
+    output zero_flag
+);
+
+    // Declaring internal wires that carry data
+    wire [31:0]read_data1;
+    wire [31:0]read_data2;
+    wire [31:0]write_data;
+
+    // Instantiating the register file
+    Register_file reg_file_module(
+    read_reg_num1,
+    read_reg_num2,
+    write_reg,
+    write_data,
+    read_data1,
+    read_data2,
+    regwrite,
+    clock,
+    reset
+    );
+
+    // Instanting ALU
+    ALU alu_module(read_data1, read_data2, alu_control, write_data, zero_flag);
+
+endmodule
---
+++ Datapath/Datapath_tb.v
@@ -0,0 +1,58 @@
+`include "Datapath.v"
+
+module stimulus ();
+
+    reg [4:0] read_reg_num1;
+    reg [4:0] read_reg_num2;
+    reg [4:0] write_reg;
+    reg [3:0] alu_control;
+    reg regwrite;
+    reg clock;
+    reg reset;
+
+    Datapath test_module( 
+     read_reg_num1,
+     read_reg_num2,
+     write_reg,
+     alu_control,
+     regwrite,
+     clock,
+     reset,
+     zero_flag);
+
+    initial begin
+        $dumpfile("output_wave.vcd");
+        $dumpvars(0,stimulus);
+    end
+
+    initial begin
+        reset = 1;
+        #20 reset = 0;
+    end
+
+    initial begin
+        #25 read_reg_num1 = 0; read_reg_num2 = 0;
+        #20 read_reg_num1 = 0; read_reg_num2 = 1;
+        #20 read_reg_num1 = 0; read_reg_num2 = 1;
+        #20 read_reg_num1 = 1; read_reg_num2 = 2;
+    end
+
+    initial begin
+        regwrite = 0;
+        #10
+        regwrite = 1;
+    end
+
+    initial begin
+        alu_control = 4'b0010;
+    end
+
+    initial begin
+        clock = 0;
+        forever #10 clock = ~clock;
+    end
+
+    initial
+        #200 $finish;
+    
+endmodule
